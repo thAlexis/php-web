37db94476ce1c7578a6858b7a9c46e4b8f37567d
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-06-23 16:15:28 +0200

    Récupération de données avec REQUEST

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 <body>
   <h1 class="text-secondary">Page d'accueil</h1>
   <p>Bienvenue dans notre application PHP</p>
+  <a href="utilisateur.php?username=Mitroglou&ville=Marseille&age=55">
+    Connaitre un peu plus sur la légende ?
+  </a>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
--- utilisateur.php
+++ utilisateur.php
@@ -1,12 +1,12 @@
 <?php
 $username = "Doe";
-if ($_POST['username'] != "") {
-  $username = $_POST['username'];
+if ($_REQUEST['username'] != "") {
+  $username = $_REQUEST['username'];
 };
-$age = $_POST['age'];
-$ville = $_POST['ville'];
+$age = $_REQUEST['age'];
+$ville = $_REQUEST['ville'];
 $genre = "madame";
-if (isset($_POST['genre']) and $_POST['genre'] == 'homme') {
+if (isset($_REQUEST['genre']) and $_REQUEST['genre'] == 'homme') {
   $genre = "monsieur";
 };
 
