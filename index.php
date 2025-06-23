3d0eb155541bc2f726e56caab11d512ad9b05b20
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-06-23 11:52:57 +0200

    préparation de la page d'accueil

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home page</title>
+</head>
+
+<body>
+  <h1>Page d'accueil</h1>
+  <p>Bienvenue dans notre application PHP</p>
+</body>
+
+</html>
